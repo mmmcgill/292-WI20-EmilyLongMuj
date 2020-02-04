b8bfcf9890804caf76f5dafeb35b58fa4cb74e8e
Mujtaba Hassan <smhassan@knox.edu>
2020-02-04 13:49:35 -0600

    Revert previous version

---
+++ CS292-Template/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject GameScreen;
+    public GameObject TitleScreen;
+    
+    public void OpenPanel()
+    {
+        /*if(GameScreen != null)
+        {
+            bool isActive = GameScreen.activeSelf;
+            GameScreen.SetActive(!isActive);
+        }*/
+        GameScreen.SetActive(true);
+        TitleScreen.SetActive(false);
+    }
+  
+}
