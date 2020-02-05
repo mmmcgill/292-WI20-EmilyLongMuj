d7c93f87eac12acced28f9d554097ec098d1894a
Long Le <ldleknox.edu>
2020-02-05 12:58:14 -0600

    Feb 5th commit

---
+++ CS292-Template/Assets/Scripts/BlazeFoxController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BlazeFoxController : MonoBehaviour
+{
+
+    private float dirX;
+    public int maxHealth = 5;
+    private int curHealth;
+    public int moveSpeed = 100;
+    private float countdown;
+    
+    private Rigidbody2D RigidBody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody2d = GetComponent<Rigidbody2D>();
+        curHealth = maxHealth;      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(moveSpeed == 1000){
+           countdown -= Time.deltaTime;
+       }
+       if(countdown < 0 ){
+           moveSpeed = 100; 
+       }
+       dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+       RigidBody2d.velocity = new Vector2(dirX, 0f);
+    }
+
+    public void ChangeSpeed(){
+        
+        moveSpeed = 1000;
+        countdown = 5;
+    }
+
+    void changeHealth(int amount){
+        curHealth -= amount;
+        GUIHealthBar.instance.SetValue(curHealth);
+    }
+
+    public int getHealth(){
+        return curHealth;
+    }
+    
+}
---
+++ CS292-Template/Assets/Scripts/RetryButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetryButton : MonoBehaviour
+{
+	public GameObject PanelToOpen;
+	public GameObject CurrentPanel;
+
+	public void OpenPanel()
+	{
+		if (PanelToOpen != null)
+		{
+			PanelToOpen.SetActive(true);
+			CurrentPanel.SetActive(false);
+		}
+	}
+}
