6209c66193ebc9b924759e38aaab564a1039c568
Long Le <ldleknox.edu>
2020-02-08 14:19:21 -0600

    reset

--- CS292-Template/Assets/Scripts/PlayButton.cs
+++ CS292-Template/Assets/Scripts/PlayButton.cs
@@ -1,4 +1,31 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject GameScreen;
+    public GameObject TitleScreen;
+    
+    public void OpenPanel()
+    {
+        /*if(GameScreen != null)
+        {
+            bool isActive = GameScreen.activeSelf;
+            GameScreen.SetActive(!isActive);
+        }*/
+        GameScreen.SetActive(true);
+        TitleScreen.SetActive(false);
+    }
+  
+}
+=======
+﻿using System.Collections;
+=======
 ﻿/*using System.Collections;
+>>>>>>> PrototypeOne
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,4 +40,8 @@ public class PlayButton : MonoBehaviour
         }
     }
 }
+<<<<<<< HEAD
+>>>>>>> Long
+=======
 */
+>>>>>>> PrototypeOne
