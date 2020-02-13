fdefe945fd73c31f1d64bbbe3dd21c6b38c830c1
Long Le <ldleknox.edu>
2020-02-12 19:20:52 -0600

    add functionality

--- CS292-Template/Assets/Scripts/projectile Script/Spawner.cs
+++ CS292-Template/Assets/Scripts/projectile Script/Spawner.cs
@@ -13,10 +13,10 @@ public class Spawner : MonoBehaviour
     
     public float minTimeBetweenSpawns;
     public float decrease;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -25,7 +25,9 @@ public class Spawner : MonoBehaviour
         if(timeBtwSpawns <= 0){
             Transform randomSpawnPoint = SpawnPoint[Random.Range(0, SpawnPoint.Length)];
             GameObject randomProjectile = projectile[Random.Range(0, projectile.Length)];
+            //note: can't work on position for now: work on homing position instead
             GameObject spawnling = Instantiate(randomProjectile, randomSpawnPoint.position, Quaternion.identity) as GameObject;
+            
             spawnling.transform.SetParent(Panel.transform, false);
 
             if(startTimeBtwSpawns > minTimeBetweenSpawns){
